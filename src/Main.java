9cae20df97345c411d20c6ca7a19f24d61f38f3f
Vladimir Muraito <vl.muraito@mail.ru>
2024-04-29 01:14:42 +1000

    Прошу проверить ДЗ.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("\nTask 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+
+        String fullName = String.format("%s %s %s", lastName, firstName, middleName);
+        System.out.println("«Ф. И. О. сотрудника — " + fullName + "».");
+
+        System.out.println("\nTask 2");
+        System.out.printf("«Данные Ф. И. О. сотрудника для заполнения отчета — %s».%n",fullName.toUpperCase());
+
+        System.out.println("\nTask 3");
+        fullName = "Иванов Семён Семёнович";
+        System.out.println("«Данные Ф. И. О. сотрудника — " +
+                fullName.replace("ё", "е").replace("Ё", "Е") + "».");
     }
 }
